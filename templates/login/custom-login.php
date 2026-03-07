18cae9bf716f06c9f02d0477e34525c513f42274
Cesar <ccabanillasd@vcstudioperu.com>
2026-03-07 12:20:21 +0100

    login edit

--- templates/login/custom-login.php
+++ templates/login/custom-login.php
@@ -5,17 +5,22 @@ if (!defined('ABSPATH')) exit;
   <div class="vc-login-card">
     <?php if (!empty($is_logged_in)): ?>
       <div class="vc-login-header">
-        <h1 class="vc-login-title">You are already logged in</h1>
-        <p class="vc-login-subtitle">Go to your account to continue.</p>
+        <p class="h3">You are already logged in</p>
+        <p class="subtitle">Go to your account to continue.</p>
       </div>
 
       <a class="vc-login-btn" href="<?php echo esc_url($account_url); ?>">Go to account</a>
     <?php else: ?>
       <div class="vc-login-header">
-        <h1 class="vc-login-title">Welcome back</h1>
-        <p class="vc-login-subtitle">Sign in to access your account.</p>
+        <p class="h3">Welcome back</p>
+        <p class="subtitle">Sign in to access your account.</p>
       </div>
+<a class="button button-google" href="<?php echo esc_url($google_login_url); ?>">
+  <img src="<?php echo esc_url($google_logo_url); ?>" alt="Aeropro">
+  Continue with Google
+</a>
 
+<div class="divider"><span>o</span></div>
       <div class="vc-login-form-wrap">
         <?php echo $pmpro_login_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
       </div>
